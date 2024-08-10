d42c8e4c786bbe0bac30f4bc905e4bd5f11a6eb7
rasheedaldeb <rasheedaldeb@gmail.com>
2024-08-11 01:58:52 +0300

    done

--- src/assets/Components/Education/Education.jsx
+++ src/assets/Components/Education/Education.jsx
@@ -1,30 +1,47 @@
-import { useContext } from "react"
-import {ThemeContext} from '../../Context/ThemContext'
-const Education = ({cards}) => {
-    const {mode2, mode3} = useContext(ThemeContext)
-    return (
-        <div id='educat' className={` ${mode2} pt-10 flex flex-col items-center
-        gap-10 md:gap-14 md:pt-16 text-primery`}>
-        <h1 className='text-2xl font-bold  md:text-5xl'>
-        Education and <span className={` ${mode2} text-secondery`}>Courses</span></h1>
-        <div className="education-cards text-primery px-3 flex flex-col gap-10 md:flex-row
-        flex-wrap justify-center items-center">
-            {cards.map((item, index)=>{
-                return(
-                    <div className={` ${mode3} ${mode2} card p-7 rounded-lg flex flex-col items-start gap-3 md:gap-5`}
-                    style={{maxWidth:"400px"
-                    }} key={index}>
-                        <h2 className='text-xl font-bold  md:text-2xl'>{item.course}</h2>
-                        <h3 className='font-bold  md:text-lg'>{item.detail}</h3>
-                        <p className=' md:text-lg md:leading-8'>{item.subtitle}</p>
-                        <p className=''>Damascus: <span className='bg-secondery p-2 rounded'
-                        style={{color: "#fff"}}>{item.date}</span></p>
-                    </div>
-                )
-            })}
-        </div>
-        </div>
-    )
-}
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { ThemeContext } from "../../Context/ThemContext";
+const Education = ({ cards }) => {
+  const { mode2, mode3 } = useContext(ThemeContext);
+  return (
+    <div
+      id="educat"
+      className={` ${mode2} pt-10 flex flex-col items-center
+        gap-10 md:gap-14 md:pt-16 text-primery`}
+    >
+      <h1 className="text-2xl font-bold  md:text-5xl">
+        Education and{" "}
+        <span className={` ${mode2} text-secondery`}>Courses</span>
+      </h1>
+      <div
+        className="education-cards text-primery px-3 flex flex-col gap-10 md:flex-row
+        flex-wrap justify-center items-center"
+      >
+        {cards.map((item, index) => {
+          return (
+            <div
+              className={` ${mode3} ${mode2} card p-7 rounded-lg flex flex-col items-start gap-3 md:gap-5`}
+              style={{ maxWidth: "400px" }}
+              key={index}
+            >
+              <h2 className="text-xl font-bold  md:text-2xl">{item.course}</h2>
+              <h3 className="font-bold  md:text-lg">{item.detail}</h3>
+              <p className=" md:text-lg md:leading-8">{item.subtitle}</p>
+              <p className="">
+                Damascus:{" "}
+                <span
+                  className="bg-secondery p-2 rounded"
+                  style={{ color: "#fff" }}
+                >
+                  {item.date}
+                </span>
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Education
+export default Education;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      'primery' : '#445964',
-      'secondery' : '#263138',
-      'hover' : '#fff'
+      primery: "#445964",
+      secondery: "#263138",
+      hover: "#fff",
     },
     width: {
-      'mobil' : '400px',
-      'input_big' : '550px',
-      'input_small' : '250px'
+      mobil: "400px",
+      input_big: "550px",
+      input_small: "250px",
     },
     height: {
-      'mobil': '350px'
+      mobil: "350px",
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
