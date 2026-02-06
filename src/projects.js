a2c4700fbae687f4ad7470ccfa63f645e3e7e88d
rasheedaldeb@gmail.com <rasheedaldeb@gmail.com>
2026-02-06 21:10:38 +0300

    project added

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,15 @@
 export const projects = [
   {
     id: 1,
+    title: "Deved Up",
+    description:
+      "We are an architecture and interior design studio focused on creating functional, elegant, and timeless spaces. Our work combines creativity, technical expertise, and attention to detail to deliver designs that reflect each client’s vision.From concept to completion, we develop thoughtful solutions for residential and commercial projects, ensuring quality, efficiency, and lasting value in every space we create.",
+    image: "/projects/deved-up.png",
+    tags: "Next.js App Route, TailwindCss, Swiper, MUI, i18n, Responsive Design",
+    demoUrl: "https://deved-up.com",
+  },
+  {
+    id: 2,
     title: "The General Authority for Tourism and Hotel Training in Syria.",
     description:
       "This website showcases The General Authority for Tourism and Hotel Training in Syria, highlighting its training programs, achievements, and contributions to the tourism and hospitality sector. It provides a clear overview of the Authority’s mission, projects, and impact.",
@@ -9,7 +18,7 @@ export const projects = [
     demoUrl: "https://gc-tht.edu.sy/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Cinpaa Training",
     description:
       "Developed an intuitive and responsive training website using React.js. The platform was designed to deliver a seamless learning experience, facilitate easy course enrollment, and provide comprehensive course management features.",
@@ -18,7 +27,7 @@ export const projects = [
     demoUrl: "http://front-end.cinpaa.org/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Evogym",
     description:
       "dynamic and engaging fitness website designed to help users achieve their health and wellness goals. Built with React and TypeScript, FitLife offers a seamless and interactive user experience, ensuring that users can easily navigate through various features and resources.",
@@ -28,7 +37,7 @@ export const projects = [
   },
 
   {
-    id: 4,
+    id: 5,
     title: "Akarat Plus Company Dashboard",
     description:
       "A sleek and powerful dashboard built with React,designed to enable real estate companies toefficiently manage their operations and enhancedecision-making. The dashboard provides anintuitive interface with advanced analytics andmanagement features.",
@@ -37,7 +46,7 @@ export const projects = [
     demoUrl: "https://company-graduation.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "Macc-Essential",
     description:
       "E-commerce Web Application, modern, user-friendly platform designed to provide a seamless shopping experience. Built with React.js, the application offers features such as product browsing, shopping cart management, and secure checkout.",
@@ -46,7 +55,7 @@ export const projects = [
     demoUrl: "https://company-graduation.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     title: "Abaq-Perfume",
     description:
       "Discover a world where fragrance tells a story. At Abaq, we believe every scent embodies an emotion, a memory, and a moment. Explore our curated collection of luxurious perfumes designed to captivate and inspire.",
@@ -55,7 +64,7 @@ export const projects = [
     demoUrl: "https://abaq-perfume.com/",
   },
   {
-    id: 7,
+    id: 8,
     title: "OFT",
     description:
       "The React-built financial company website features a professional home page, sections like About Us and Services, a blog with articles and market analysis, a resources section with tools, and a contact page.",
@@ -64,7 +73,7 @@ export const projects = [
     demoUrl: "https://ownfinencialtactics.netlify.app",
   },
   {
-    id: 8,
+    id: 9,
     title: "ShopCart E-commerce",
     description:
       "Crafted with the power of React and the elegance of Bootstrap, this e-commerce platform offers a seamless and engaging shopping experience. Designed to showcase a wide range of products, the website is fully responsive and optimized for performance",
@@ -73,30 +82,12 @@ export const projects = [
     demoUrl: "https://e-commerce-react-beta-ecru.vercel.app/",
   },
   {
-    id: 9,
+    id: 10,
     title: "B-Book",
     description:
       "dynamic and user-friendly book browsing website built with React. The platform offers an extensive collection of books across various genres, allowing users to discover, explore, and enjoy their favorite reads with ease.",
     image: "/projects/book.png",
     tags: "React, react-router ,axios",
     demoUrl: "https://books-website-coral.vercel.app/",
-  },
-  {
-    id: 10,
-    title: "Shopping",
-    description:
-      "Shopping Cart Web Application is an interactive web-based application designed to provide users with a seamless and intuitive shopping experience. The application allows users to browse products, add them to a cart, and review their selections before proceeding to checkout.",
-    image: "/projects/shopping.png",
-    tags: "Html, Css, JavaScript",
-    demoUrl: "https://rasheedaldeb.github.io/shoping-cart/",
-  },
-  {
-    id: 11,
-    title: "Dashboard",
-    description:
-      "admin dashboard project demonstrates my ability to create a dynamic and intuitive user interface for managing various business functions. It features secure authentication, role-based access, comprehensive user activity tracking, interactive data visualizations.",
-    image: "/projects/dashboard.png",
-    tags: "React , MUI, recharts, sass",
-    demoUrl: "https://admin-dashboard-kappa-ivory.vercel.app/",
   },
 ];
